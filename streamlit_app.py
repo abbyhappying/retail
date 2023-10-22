42db8d7f8404455e3ed78ed07a1eeed2545add27
abbyhappying <133288003+abbyhappying@users.noreply.github.com>
2023-10-22 21:11:00 +0000

    update openAI API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit as st
+from langchain.callbacks import StreamlitCallbackHandler
 from langchain.llms import OpenAI
 from snowflake.snowpark import Session
 from snowflake.connector import connect  # Import the necessary module
@@ -39,10 +39,8 @@ db = SQLDatabase.from_uri(conn_string)
 # session = create_session()
 # st.success("Connected to Snowflake!")
 
-
-llm = OpenAI(api_key=st.secrets.openai_api_key,temperature=0, verbose=True)
-openai_api_key = st.secrets.openai_api_key
-
+openai_api_key = st.secrets.OPENAI_API_KEY
+llm = OpenAI(api_key=st.secrets.OPENAI_API_KEY,temperature=0, verbose=True)
 
 #generate longchain sql agent to interact with the snowflake database
 agent_executor = create_sql_agent(
@@ -55,3 +53,30 @@ agent_executor = create_sql_agent(
 
 # agent_executor.run("how was the number of total orders")
 # agent_executor.run("how was the top 3 selling products")
+
+# Initialize the chat message history
+if "messages" not in st.session_state.keys(): 
+    welcome_message = 'Welcome!'
+    st.session_state.messages = [
+        {"role": "assistant", "content": welcome_message}
+    ]
+
+# Prompt for user input and save to chat history
+if prompt := st.chat_input("Your question"): 
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# Display the prior chat messages
+for message in st.session_state.messages: 
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Hmm..."):
+            st_callback = StreamlitCallbackHandler(st.container())
+            response = agent_executor.run(prompt, callbacks=[st_callback])
+            
+            message = {"role": "assistant", "content": response} 
+            st.write(response) # write the message into the chat history
+            st.session_state.messages.append(message) # display the message
